98d64a0be141d5b78e32fbbe02f649b4b1089bd5
Joakim Almgren <joakim.oa@gmail.com>
2022-12-11 11:30:10 +0100

    added solution for day 10 part 2

--- lib/day10/day10.ex
+++ lib/day10/day10.ex
@@ -36,4 +36,46 @@ defmodule Day10 do
     dbg sum
     dbg "hello"
   end
+
+  def part_two() do
+    instructions = load_input("./lib/day10/input.txt")
+    |> Enum.map(fn x ->
+      case x do
+       "noop" -> {:noop, 0, 1}
+        _ -> {:addx, String.to_integer(Enum.at(String.split(x),1)), 2}
+      end
+    end)
+    dbg instructions
+
+    # acc = {cycle, register, %([cycle => register, ...])}, if it's a noop we add one elem to mapset, if a addx we add two elem to it
+    {cyc, reg, regmap} = List.foldl(instructions, {0, 1, %{}}, fn ins, {cycle, register, registermap} ->
+      case ins do
+        {:noop, _, _} -> {cycle+1, register, Map.put(registermap, cycle, register)}
+        {:addx, val, _} ->
+          u_registermap = Map.put(registermap, cycle, register);
+          u_register = register + val;
+          uu_registermap = Map.put(u_registermap, cycle+1, u_register);
+          {cycle+2, u_register, uu_registermap}
+      end
+    end)
+    dbg {cyc, reg, regmap}
+
+    dbg Map.get(regmap, 0)
+    dbg Map.get(regmap, 1)
+
+    screenoutput = Enum.to_list(0..239)
+    |> List.foldl('#', fn x, output ->
+      {cyc, reg} = {x+1, Map.get(regmap, x)};
+      dbg {row, hpos} = {div(cyc, 40), reg};
+      overlap = hpos - rem(cyc, 40);
+      case (overlap > 1) or (overlap < -1) do
+          true -> output++'.'
+          false -> output++'#'
+      end
+    end)
+    dbg screenoutput
+
+    chunk = screenoutput |> Enum.chunk_every(40)
+    Enum.each(chunk, &IO.puts(&1))
+  end
 end
